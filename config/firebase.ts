fb00e1c5592512118ce45cdecef9185191be8ea5
itsllyaz <eliyasinbox1@gmail.com>
2026-04-02 16:58:08 +0300

    bug: minor fix

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,16 +3,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyATG9P3Jg8uy_1nSe-Ufgg1trp4og_4E2I",
+//   authDomain: "llyaz-me.firebaseapp.com",
+//   projectId: "llyaz-me",
+//   storageBucket: "llyaz-me.firebasestorage.app",
+//   messagingSenderId: "426878077669",
+//   appId: "1:426878077669:web:80d8488ff7295686c7620c",
+//   measurementId: "G-X29NE54FR9"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyATG9P3Jg8uy_1nSe-Ufgg1trp4og_4E2I",
-  authDomain: "llyaz-me.firebaseapp.com",
-  projectId: "llyaz-me",
-  storageBucket: "llyaz-me.firebasestorage.app",
-  messagingSenderId: "426878077669",
-  appId: "1:426878077669:web:80d8488ff7295686c7620c",
-  measurementId: "G-X29NE54FR9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
