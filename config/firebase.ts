5ce1dbe11a7b201eb805dfc686f335f0df7a7299
llyaz <90453187+itsllyaz@users.noreply.github.com>
2026-04-22 22:25:19 +0300

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,15 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyATG9P3Jg8uy_1nSe-Ufgg1trp4og_4E2I",
-//   authDomain: "llyaz-me.firebaseapp.com",
-//   projectId: "llyaz-me",
-//   storageBucket: "llyaz-me.firebasestorage.app",
-//   messagingSenderId: "426878077669",
-//   appId: "1:426878077669:web:80d8488ff7295686c7620c",
-//   measurementId: "G-X29NE54FR9"
-// };
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
